ef1c17ca3230c5a5cfbe66062d018717f839f16a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-02-06 23:17:42 +0900

    tasks_specにシナリオを記載

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  scenario "user toggles a task", js: true do
+    user = FactoryBot.create(:user)
+    project = FactoryBot.create(:project,
+      name: "RSpec tutorial",
+      owner: user
+    )
+    task = project.tasks.create!(name: "Finish RSpec tutorial")
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    click_link "RSpec tutorial"
+    check "Finish RSpec tutorial"
+
+    expect(page).to have_css "label#task_#{task.id}.completed"
+    expect(task.reload).to be_completed
+
+    uncheck "Finish RSpec tutorial"
+
+    expect(page).to_not have_css "label#{task.id}.completed"
+    expect(task.reload).to_not be_completed
+  end
 end
