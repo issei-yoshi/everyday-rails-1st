75fa048913061b702235f227e20674c2589f147e
issei-yoshi <iyoshi.engineer@gmail.com>
2023-02-06 21:05:19 +0900

    projectsをtraitに書き換え

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -6,24 +6,16 @@ FactoryBot.define do
     association :owner
   end
 
-  factory :project_due_yesterday, class: Project do
-    sequence(:name) { |n| "Test Project#{n}"}
-    description { "Sample project for testing purposes" }
+  trait :due_yesterday do
     due_on { 1.day.ago }
-    association :owner
   end
 
-  factory :project_due_today, class: Project do
-    sequence(:name) { |n| "Test Project#{n}"}
-    description { "Sample project for testing purposes" }
+  trait :due_today do
     due_on { Date.current.in_time_zone }
-    association :owner
   end
 
-  factory :project_due_tomorrow, class: Project do
-    sequence(:name) { |n| "Test Project#{n}"}
-    description { "Sample project for testing purposes" }
+  trait :due_tomorrow do
     due_on { 1.day.from_now }
-    association :owner
   end
+
 end
