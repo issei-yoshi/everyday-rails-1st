7fdee964bcb9f1d003750e31d343bf4856df9923
issei-yoshi <iyoshi.engineer@gmail.com>
2023-02-06 20:25:13 +0900

    Fix

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-
   before do
     @user = User.create(
       first_name: "Joe",
@@ -10,40 +9,53 @@ RSpec.describe Note, type: :model do
       password:   "dottle-nouveau-pavilion-tights-furze",
     )
 
+
     @project = @user.projects.create(
       name: "Test Project",
     )
   end
 
+
   it "is valid with a user, project, and message" do
     note = Note.new(
-      message: "This is a sample note",
+      message: "This is a sample note.",
       user: @user,
-      project: @project
+      project: @project,
     )
+    expect(note).to be_valid
   end
 
+
   it "is invalid without a message" do
-      note = Note.new(message: nil)
-      note.valid?
-      expect(note.errors[:message]).to include("can't be blank")
+    note = Note.new(message: nil)
+    note.valid?
+    expect(note.errors[:message]).to include("can't be blank")
   end
+
 
   describe "search message for a term" do
     before do
       @note1 = @project.notes.create(
-        message: "This is the first note",
+        message: "This is the first note.",
         user: @user,
       )
       @note2 = @project.notes.create(
-        message: "This is the second note",
+        message: "This is the second note.",
         user: @user,
       )
       @note3 = @project.notes.create(
-        message: "First, preheat the oven",
+        message: "First, preheat the oven.",
         user: @user,
       )
     end
+
+
+    context "when a match is found" do
+      it "returns notes that match the search term" do
+        expect(Note.search("first")).to include(@note1, @note3)
+      end
+    end
+
 
     context "when no match is found" do
       it "returns an empty collection" do
@@ -51,5 +63,4 @@ RSpec.describe Note, type: :model do
       end
     end
   end
-
 end
