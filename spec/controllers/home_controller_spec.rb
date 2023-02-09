e98fe97d90267705f425467165f025aaa522caa9
issei-yoshi <iyoshi.engineer@gmail.com>
2023-02-09 22:09:22 +0900

    Fix:修正

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Home page", type: :request do
-  it "responds successfully" do
-    get root_path
-    expect(response).to be_successful
-    expect(response).to have_http_status "200"
+RSpec.describe HomeController, type: :controller do
+  describe "#index" do
+    it "responds successfully" do
+      get :index
+      expect(response).to be_successful
+    end
   end
 end
