742f966c0c49c3db8a97611a07ed8e0cd40f8545
issei-yoshi <iyoshi.engineer@gmail.com>
2023-02-09 22:08:17 +0900

    home_controller_specのコントローラスペックをリクエストスペックに修正

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe HomeController, type: :controller do
-  describe "#index" do
-    it "responds successfully" do
-      get :index
-      expect(response).to be_successful
-    end
+RSpec.describe "Home page", type: :request do
+  it "responds successfully" do
+    get root_path
+    expect(response).to be_successful
+    expect(response).to have_http_status "200"
   end
 end
